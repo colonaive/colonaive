620b6642e4b3b78391e45972b4ba76ae960748a7
colonaive <admin@saversmed.com>
2025-04-20 17:25:48 +0800

    Remove base path from Vite config for GoDaddy upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // 👈 THIS is the missing line that fixes the blank screen
+  plugins: [react()]
 })
